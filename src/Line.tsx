1c63b01257cc2bba6e8054b047e435714ca01abf
gigsy-bloom <82524667+gigsy-bloom@users.noreply.github.com>
2021-05-12 19:09:55 +0530

    Update Line.tsx

--- src/Line.tsx
+++ src/Line.tsx
@@ -22,7 +22,7 @@ type Props = {
   /** Component to render tooltips. An example component is included: <Tooltip />. */
   tooltipComponent?: JSX.Element
   /** Callback method that fires when a tooltip is displayed for a data point. */
-  onTooltipSelect?: (value: ChartDataPoint, index: number) => void
+  onTooltipSelect?: (value: String, index: number) => void
   /** Callback method that fires when the user stopped touching the chart. */
   onTooltipSelectEnd?: () => void
   /** Set to true if the tooltip should be hidden immediately when the user stops dragging the chart. */
